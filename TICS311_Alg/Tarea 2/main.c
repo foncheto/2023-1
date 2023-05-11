d171a3d826a36175c7c5919bab2893112bd8e667
foncheto <81327113+foncheto@users.noreply.github.com>
2023-05-11 17:38:31 -0400

    Update main.c

--- TICS311_Alg/Tarea 2/main.c
+++ TICS311_Alg/Tarea 2/main.c
@@ -5,13 +5,17 @@
 #define MAX_LINE_LENGTH 1024
 #define MAX_USERS 1000
 
+// FALTA HACER QUICKSORT O MERGESORT PARA ORDENAR LOS USUARIOS POR EDAD
+
 typedef struct {
     char nombre[100];
     int edad;
 } Usuario;
 
-void merge_sort(int arreglo[], int N);
-void merge_sort_aux(int *arreglo, int ipos, int fpos);
+void quick_sort_aux(int *arreglo, int ipos, int fpos);
+int partition(int *arreglo, int ipos, int fpos);
+void quick_sort(Usuario *arreglo, int N);
+
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
@@ -66,4 +70,62 @@ int main(int argc, char *argv[]) {
     }
 
     return 0;
+}
+
+void quick_sort_aux(int *arreglo, int ipos, int fpos);
+
+// La funcion partition particiona el subarreglo entre las posiciones [ipos...fpos]
+// El pivote es el primer elemento del subarreglo, es decir arreglo[ipos]
+// Se deja el pivote en su posicion correcta, 
+// al lado izquierdo quedan los menores que el pivote, al lado derecho los mayores que el pivote
+// Ojo: En este implementacion los subarreglos izquierdo y derecho pueden quedar en cualquier orden internamente
+// Lo importante es que el subarreglo izquiero tienen los menores al pivote y el derecho los mayores
+// Ojo: La funcion modifica el arreglo de entrada (NO entrega un nuevo arreglo)
+int partition(int *arreglo, int ipos, int fpos);
+
+// Quick Sort
+// Utilizamos la funcion auxiliar quick_sort_aux para poder aplicar recursion de manera mas comoda
+// Ordenamos el arreglo entero, es decir, llamamos a quick_sort_aux con posiciones 0 y N-1
+// Esta implementacion modifica el arreglo de entrada (NO entrega un nuevo arreglo)
+void quick_sort(Usuario *arreglo, int N)
+{
+  quick_sort_aux(arreglo, 0, N-1);
+}
+
+// Implementacion recursiva de quick_sort_aux
+// Notar que el caso base es cuando ipos >= fpos
+void quick_sort_aux(int *arreglo, int ipos, int fpos)
+{
+  if (ipos >= fpos) {
+    return;
+  }
+
+  int pivot_pos = partition(arreglo, ipos, fpos);
+  quick_sort_aux(arreglo, ipos, pivot_pos-1);
+  quick_sort_aux(arreglo, pivot_pos+1, fpos);
+}
+
+// Implementacion de partition
+int partition(int *arreglo, int ipos, int fpos) 
+{
+  int aux;
+  int pivot = arreglo[ipos]; // el pivote es el primer elemento
+  int i = ipos + 1; // el i mantiene la posicion donde deberia ir el proximo elemento menor al pivote
+  for(int j = ipos + 1; j <= fpos; j++) {
+    if (arreglo[j] < pivot) {
+      // swap entre arreglo[i] y arreglo[j]
+      aux = arreglo[i];
+      arreglo[i] = arreglo[j];
+      arreglo[j] = aux;
+      // es importante actualizar i
+      i++;
+    }
+  }
+
+  // la posicion i-1 es la posicion correcta del pivote
+  // hacemos swap entre arreglo[i-1] y el pivote, es decir, arreglo[ipos]
+  arreglo[ipos] = arreglo[i-1];
+  arreglo[i-1] = pivot;
+
+  return i-1;
 }
