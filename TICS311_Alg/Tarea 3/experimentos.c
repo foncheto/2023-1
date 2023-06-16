6d4ce8f0c0f62ebf63005ca5422da0f97a02f059
foncheto <81327113+foncheto@users.noreply.github.com>
2023-06-15 23:42:57 -0400

    update win

--- TICS311_Alg/Tarea 3/experimentos.c
+++ TICS311_Alg/Tarea 3/experimentos.c
@@ -8,4 +8,55 @@
 int main(int argc, char *argv[])
 {	
   // Haga aca sus experimentos de listas y arboles
+   int arr1[100], arr2[1000], arr3[10000], arr4[100000], arr5[200000],
+      arr6[500000];
+
+  // llenar los arreglos con números aleatorios
+  for (int i = 0; i < 100; i++) {
+    arr1[i] = rand() % 1000;
+  }
+
+  for (int i = 0; i < 1000; i++) {
+    arr2[i] = rand() % 1000;
+  }
+
+  for (int i = 0; i < 10000; i++) {
+    arr3[i] = rand() % 1000;
+  }
+
+  for (int i = 0; i < 100000; i++) {
+    arr4[i] = rand() % 1000;
+  }
+
+  for (int i = 0; i < 200000; i++) {
+    arr5[i] = rand() % 1000;
+  }
+
+  for (int i = 0; i < 500000; i++) {
+    arr6[i] = rand() % 1000;
+  }
+  nodo_lista *lista1 = NULL; //nodo inicial de la lista
+  nodo_arbol *arbol1 = NULL; //raiz del arbol
+
+  // Insertar elementos del arreglo en la lista y medir tiempo
+  clock_t inicio_lista = clock();
+  for (int i = 0; i < 100; i++) {
+      insertar_final(&lista1, arr1[i]);
+  }
+  clock_t fin_lista = clock();
+  double tiempo_lista = ((double)(fin_lista - inicio_lista)) / CLOCKS_PER_SEC;
+  printf("Tiempo total de inserción en lista (100 elementos): %.6f segundos\n", tiempo_lista);
+
+  // Insertar elementos del arreglo en el árbol y medir tiempo
+  clock_t inicio_arbol = clock();
+  for (int i = 0; i < 100; i++) {
+      insertar_nodo(&arbol1, arr1[i]);
+  }
+  clock_t fin_arbol = clock();
+  double tiempo_arbol = ((double)(fin_arbol - inicio_arbol)) / CLOCKS_PER_SEC;
+  printf("Tiempo total de inserción en árbol (100 elementos): %.6f segundos\n", tiempo_arbol);
+
+  // Continuar con los demás arreglos y mediciones
+
+  return 0;
 }
--- TICS311_Alg/Tarea 3/listas.c
+++ TICS311_Alg/Tarea 3/listas.c
@@ -1,11 +1,25 @@
 #include "listas.h"
-
 // Inserta el valor data al comienzo de la lista
 // Retorna un puntero al primer nodo de la nueva lista
 // OJO: el elemento se debe insertar al comienzo de la lista
-nodo_lista *insertar_lista(nodo_lista *lista, int data)
+nodo_lista *insertar_lista(nodo_lista **lista, int data)
 {
-    
+    if (*ref_lista == NULL) { // caso lista vacia
+        nodo *nuevo_nodo = (nodo *)malloc(sizeof(nodo));
+        nuevo_nodo->x = valor; 
+        nuevo_nodo->next = NULL; 
+        *ref_lista = nuevo_nodo;  // aca es donde modificamos la variable lista
+    }
+    else { // caso lista no vacia
+        nodo *it = *ref_lista;
+        while(it->next != NULL) { // importante preguntar it->next != NULL y no it != NULL
+            it = it->next;
+        }
+        nodo *nuevo_nodo = (nodo *)malloc(sizeof(nodo));
+        nuevo_nodo->x = valor; 
+        nuevo_nodo->next = NULL; 
+        it->next = nuevo_nodo;
+    }
 }
 
 // Busca el valor data en la lista partiendo desde el comienzo hasta el final
@@ -13,12 +27,26 @@ nodo_lista *insertar_lista(nodo_lista *lista, int data)
 // Si no, retorna NULL
 nodo_lista *buscar_lista(nodo_lista *lista, int data)
 {
-    
+    nodo_lista *nodo_actual = lista;
+    while (nodo_actual != NULL)
+    {
+        if (nodo_actual->data == data)
+        {
+            return nodo_actual;
+        }
+        nodo_actual = nodo_actual->next;
+    }
+    return NULL;
 }
     
     
 // Imprime en pantalla la lista desde el comienzo hasta el final
 void imprimir_lista(nodo_lista *lista)
 {
-    
+    nodo_lista *nodo_actual = lista;
+    while (nodo_actual != NULL)
+    {
+        printf("%d ", nodo_actual->data);
+        nodo_actual = nodo_actual->next;
+    }
 }
